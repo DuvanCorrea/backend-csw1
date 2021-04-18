cf2c9659f4cb071e0bbb9fe94fb834b3127a91a8
DuvanCorrea <duvancorrea96@gmail.com>
2021-04-17 21:44:16 -0500

    Rutas docente ok

--- src/controladores/docenteCtrl.js
+++ src/controladores/docenteCtrl.js
@@ -2,20 +2,21 @@ const db = require("../database/db")
 
 // quierys para peticiones a la base de datos
 // ------------------------------------------
-const GETONE = "SELECT clave FROM DOCENTES WHERE (id_docente=?)"
+const GETONE = "SELECT id_docente, nombre_completo, correo, anio_nacimiento, areas_conocimiento, materia FROM DOCENTES WHERE (id_docente=?)"
+const GETALL = "SELECT * FROM DOCENTES"
+const POST = "SELECT clave FROM DOCENTES WHERE (id_docente=?)"
 
 // controlador de autenticacion de usuario
 // ---------------------------------------
 const docenteCtrl = {
 
-    // metodo para autenticar usuario
-    // ----------------------------------------------------------
-
-    post: (req, res) => {
+    // Metodo para consulta un solo docente
+    // ------------------------------------
+    getUnSoloDocenteSinMaterialNiReconocimiento: (req, res) => {
 
         // se extrae la informacion enviada desde front
         // --------------------------------------------
-        const { id_docente, clave } = req.body
+        const { id_docente } = req.params
 
         db.getConnection((err, conn) => {
             if (err) {
@@ -32,7 +33,42 @@ const docenteCtrl = {
                             res.status(404)
                             res.send({ respuesta: "docente no encontrado", descripcion: "no se encontro el docente en la base de datos" })
                         } else {
+
+                            res.send(rows[0])
+
+                        }
+                    }
+                })
+            }
+        })
+
+    },
+
+    // metodo para autenticar usuario
+    // ------------------------------
+    post: (req, res) => {
+
+        // se extrae la informacion enviada desde front
+        // --------------------------------------------
+        const { id_docente, clave } = req.body
+
+        db.getConnection((err, conn) => {
+            if (err) {
+                console.log(err)
+                res.status(500)
+                res.send({ respuesta: "error", descripcion: "no se pudo conectar a la base de datos (1)" })
+            } else {
+                conn.query(POST, [id_docente], (err, rows) => {
+                    if (err) {
+                        res.status(500)
+                        res.send({ respuesta: "error", descripcion: "error al consultar la base de datos (2)" })
+                    } else {
+                        if (rows.length <= 0) {
+                            res.status(404)
+                            res.send({ respuesta: "docente no encontrado", descripcion: "no se encontro el docente en la base de datos" })
+                        } else {
                             // se hace la validacion de la contraseña
+                            // --------------------------------------
                             if (rows[0].clave === clave) {
                                 res.send({ valido: true, mensaje: "docente validado" })
                             } else {
@@ -43,8 +79,30 @@ const docenteCtrl = {
                 })
             }
         })
-    }
+    },
 
+    // Metodo para consulta todos los docentes
+    // ---------------------------------------
+    getTodosLosDocentesSinMaterialNiReconocimiento: (req, res) => {
+
+        db.getConnection((err, conn) => {
+            if (err) {
+                console.log(err)
+                res.status(500)
+                res.send({ respuesta: "error", descripcion: "no se pudo conectar a la base de datos (1)" })
+            } else {
+                conn.query(GETALL, [], (err, rows) => {
+                    if (err) {
+                        res.status(500)
+                        res.send({ respuesta: "error", descripcion: "error al consultar la base de datos (2)" })
+                    } else {
+                        res.send(rows)
+                    }
+                })
+            }
+        })
+
+    },
 
 }
 
--- src/rutas/docente.routes.js
+++ src/rutas/docente.routes.js
@@ -2,6 +2,10 @@ const express = require("express")
 const router = express.Router()
 const docenteCtrl = require("../controladores/docenteCtrl")
 
-router.post("/docente", docenteCtrl.post)
+router.get("/docente/:id_docente", docenteCtrl.getUnSoloDocenteSinMaterialNiReconocimiento)
+router.post("/docente/login", docenteCtrl.post)
+router.get("/docente", docenteCtrl.getTodosLosDocentesSinMaterialNiReconocimiento)
+
 
 module.exports = router
+
